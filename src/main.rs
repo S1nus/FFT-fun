eaa854d28342a13c15d9c68d6ce42da4a2f0c7f0
c-node <chuckhead7@gmail.com>
2026-01-16 01:17:24 -0500

    fft fun

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,40 @@ use pasta_curves::{
 };
 use rand::rngs::OsRng;
 use core::ops::Mul;
+use std::collections::HashSet;
 
 fn main() {
-    // 4 + 3x + 2x^2 + x^3 + x^4
-    let my_polynomial = vec![4, 3, 2, 1, 1];
-    let poly_num_coeffs = my_polynomial.len();
-    let generator = Point::generator();
-    let hasher = Ep::hash_to_curve("cnode");
-    let g_points: Vec<Ep> = (0..poly_num_coeffs)
-        .map(|i| {
-            let mut input = Vec::with_capacity(1 + 8);
-            input.push(b'g');
-            input.extend_from_slice(&i.to_le_bytes());
-            hasher(&input)
-        })
-        .collect();
+    let modulus: u64 = 337;
+    let root_of_unity: u64 = 85;
 
+    let mut unique_elements = HashSet::new();
+    for i in 0..modulus {
+        let element = mod_exp(root_of_unity, i, modulus);
+        unique_elements.insert(element);
+    }
+    println!("num unique elements: {}", unique_elements.len());
+
+    let domain_size: u64 = unique_elements.len() as u64;
+
+    let mut unique_squares = HashSet::new();
+    for i in 0..domain_size {
+        let element = mod_exp(root_of_unity, i, modulus);
+        let square = mod_exp(element, 2, modulus);
+        unique_squares.insert(square);
+        println!("square: {}", square);
+    }
+    println!("Num unique squares: {}", unique_squares.len());
+}
+
+fn mod_exp(mut base: u64, mut exp: u64, modulus: u64) -> u64 {
+    let mut result = 1;
+    base %= modulus;
+    while exp > 0 {
+        if exp % 2 == 1 {
+            result = result * base % modulus;
+        }
+        exp /= 2;
+        base = base * base % modulus;
+    }
+    result
 }
